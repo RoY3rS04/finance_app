fdffa58a7f4de6a8ddea833a4dde6f720e9442ac
RoYerS <rogermoises2004@gmail.com>
2024-10-09 17:13:29 -0600

    Starting the layout

--- src/Hi.tsx
+++ src/Hi.tsx
@@ -1,6 +1,6 @@
 export function Hi({ msg }: {msg: string}) {
     return (
-        <div className="h-screen w-full flex justify-center items-center">
+        <div className="h-full w-full flex justify-center items-center">
             <h1 className="text-6xl">{msg}</h1>
         </div>
     )
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Hi } from './Hi';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { AppLayout } from './Layout/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout></AppLayout>,
+    children: [
+      {
+        index: true,
+        element: <Hi msg='FinanceApp'></Hi>
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Hi msg="FinanceApp"></Hi>
+    <RouterProvider router={router}/>
   </StrictMode>
 )
