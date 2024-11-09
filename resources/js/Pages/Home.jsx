4119ee180e27cf5f043519513ee419e6cc7e3b64
RoYerS <rogermoises2004@gmail.com>
2024-11-09 15:58:13 -0600

    Setting up the project

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home({name}) {
+    return (
+        <>
+            <h1>
+                Welcome back {name}!
+            </h1>
+        </>
+    )
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function() {
+    return inertia('Home', [
+        'name' => 'Doggy'
+    ]);
+});
